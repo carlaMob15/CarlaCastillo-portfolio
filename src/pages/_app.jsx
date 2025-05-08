f10bd842e1256b4816ae5411c857d5d9bef0215e
carlaMob15 <your.email@example.com>
2025-05-08 22:21:29 +0100

    Remove fixed background div to eliminate content borders

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -23,11 +23,6 @@ export default function App({ Component, pageProps, router }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div>
       <div className="relative flex min-h-screen flex-col">
         <Header />
         <main className="flex-auto">
